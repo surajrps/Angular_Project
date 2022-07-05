daf721d5cb27ddc5f1e3e37ae46e20c4399ad208
surajrps <testac2subs@gmail.com>
2022-07-05 10:46:06 +0530

    first commit

---
+++ AngProject/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './Components/first/first.component';
+import { SecondComponent } from './Components/second/second.component';
+
+const routes: Routes = [
+  {path:'first', component:FirstComponent},
+  {path:'second', component:SecondComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
