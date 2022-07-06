e471dbb1842fabf6735bc58ccfa61de2567e8abc
surajrps <testac2subs@gmail.com>
2022-07-06 10:45:12 +0530

    Modified routing module and app component template.

--- AngProject/src/app/app-routing.module.ts
+++ AngProject/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { FirstComponent } from './Components/first/first.component';
 import { SecondComponent } from './Components/second/second.component';
 
 const routes: Routes = [
   {path:'first', component:FirstComponent},
-  {path:'second', component:SecondComponent}
+  {path:'second', component:SecondComponent},
+  { path: '**', component: FirstComponent }
 ];
 
 @NgModule({
